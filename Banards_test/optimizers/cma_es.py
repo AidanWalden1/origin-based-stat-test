92698d59f42c54ce48f64ce443a5a23f4dc80801
aiw24 <aiw24@aber.ac.uk>
2023-03-13 02:37:56 +0000

    uses self.iteration rather than globla var

--- Banards_test/optimizers/cma_es.py
+++ Banards_test/optimizers/cma_es.py
@@ -1,7 +1,6 @@
 import cma
 import numpy as np
 
-iterations = 150
 
 class CMAESOptimizer:
     def __init__(self, iterations=100):
@@ -11,12 +10,12 @@ class CMAESOptimizer:
         start_points = [np.random.uniform(low=bounds[0][0], high=bounds[0][1]), np.random.uniform(low=bounds[1][0], high=bounds[1][1])]
         options = {'maxiter': 50, 'verbose': -9, 'bounds': ([bounds[0][0], bounds[1][0]], [bounds[0][1], bounds[1][1]]), 'maxfevals': 1000}
         optimizer = cma.CMAEvolutionStrategy(start_points, 8, options)
-        for i in range(iterations):
+        for i in range(self.iterations):
             solutions = optimizer.ask()
             optimizer.tell(solutions, [obj_function(x) for x in solutions])
             optimizer.logger.add()  # write data to disc to be plotted
             optimizer.disp()
-            if i == iterations - 1:
+            if i == self.iterations - 1:
                 return solutions[np.random.randint(len(solutions))]
             
     @property
