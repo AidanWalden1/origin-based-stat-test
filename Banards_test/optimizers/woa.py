708598e503a9db8fdbed6ec2b869828dbcc96840
Aidan <56968450+AidanWalden1@users.noreply.github.com>
2023-03-19 20:07:37 +0000

    Update woa.py
    changed sigma, significantly improved performance

--- Banards_test/optimizers/woa.py
+++ Banards_test/optimizers/woa.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('c:/Users/Aidan/OneDrive/Documents/MMP/Banards_test/optimizers/')
 from woa_pkg.whale_optimization import WhaleOptimization
 import numpy as np
 
@@ -9,14 +7,16 @@ def objective_function_wrapper(x, y,obj):
     return obj(x_)
 
 class WoaOptimizer:
-    def __init__(self, iterations=100):
+    def __init__(self, iterations=30):
         self.iterations = iterations
 
 
     def woaOptimizer(self, bounds, obj):
-        # optimizer = cma.CMAEvolutionStrategy(start_points, 8, options)
         
-        woa = WhaleOptimization(opt_func=lambda x, y: objective_function_wrapper(x, y, obj), constraints=bounds, nsols=10, b=1, a=2, a_step=0.01)
+        a=2
+        a_step = a/self.iterations
+        
+        woa = WhaleOptimization(opt_func=lambda x, y: objective_function_wrapper(x, y, obj), constraints=bounds, nsols=50, b=1, a=a, a_step=a_step)
 
         for i in range(self.iterations):
             woa.optimize()
